cc8b3dc1f1357c7f88578f290bdabb0c6f4476a4
VitaliyLenin <vitaliylenin77@gmail.com>
2023-07-20 23:39:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = "6026489808:AAGZp3mA2giiHW2WfvdcCOzsRqKBJpGraO4";
+const webAppUrl =
+  "https://64b9972f18f2a03b390ffbb7--dashing-donut-1d5354.netlify.app";
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  const text = msg.text;
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
+  if (text === "/start") {
+    await bot.sendMessage(
+      chatId,
+      "Привіт,нижче з'явиться кнопка для заповнення форми",
+      {
+        reply_markup: {
+          keyboard: [
+            [{ text: "Заповніть форму", web_app: { url: webAppUrl } }],
+          ],
+        },
+      }
+    );
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, "Received your message");
+    await bot.sendMessage(
+      chatId,
+      "Привіт,нижче з'явиться кнопка для заповнення форми",
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Зробіть замовлення", web_app: { url: webAppUrl } }],
+          ],
+        },
+      }
+    );
+  }
 });
